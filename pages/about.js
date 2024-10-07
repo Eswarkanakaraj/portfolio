634e3798dee0efe09ac2647864997e1e667fc0d7
Eswarkanakaraj <eswarkanakaraj@gmail.com>
2024-10-07 16:28:01 +0530

    first commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+
+export default function About() {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>This website is made by a cool coder who loves learning new things!</p>
+       
+        <a href="Contact" className="return">Go back to Home</a>
+        
+      </div>
+    );
+  }
+  
